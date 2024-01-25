1406575524288021fe80283a9bcc95bb420f0963
Jayamurugavel <jayamurugavel9442@gmail.com>
2024-01-25 14:44:58 +0530

    for loop program added for reference

--- add.py
+++ add.py
@@ -1,4 +1,13 @@
-a=5
-b=6
-print(a+b)
+a=[]
+sum=0
+for i in range(5):
+    b=int(input("enter numer="+str(i+1)))
+    #print(b)
+    a.append(b)
+print(a)
 
+for i in a:
+    sum=sum+i
+    avg=sum//5
+print(sum)
+print(avg)
